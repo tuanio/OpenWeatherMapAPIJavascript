444c41262003aa1ff3c1f3d73c25c3e8c823c140
TuanNguyenVanAnh <nvatuan3@gmail.com>
2019-08-23 10:49:47 +0700

    optimize and add more function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// http://www.openweathermap.org/data/2.5/forecast?id=1566083&appid=b6907d289e10d714a6e88b30761fae22
+// http://www.openweathermap.org/data/2.5/forecast?id=f45ce2b4d44215c184ac50b73a96760e
 // var KEY  = "e635e6a8c683f448b994a6e254567aa7";
 
 var data;
@@ -26,7 +26,7 @@ r.onreadystatechange = data => { // fat anoynymous function using =>
 
         for(var i = 1; i <= 5; i++){
             document.getElementById("img"+ i).src = "http://openweathermap.org/img/w/" + data.list[(i-1)*8].weather[0].icon + ".png";
-            document.getElementById("img" + i).style.width = "80px";
+            document.getElementById("img" + i).style.width = "90px";
 
 
             document.getElementById("text_description" + i).innerHTML = capitalizeFirstLetter(data.list[(i-1)*8].weather[0].description);
@@ -47,16 +47,26 @@ r.onreadystatechange = data => { // fat anoynymous function using =>
 
 var place = document.getElementById("txt_place");
 var commit_place = document.getElementById("commit_place");
+var spanHumidity = document.getElementsByClassName("span_hum");
+
+place.value = 1580578;
 
 commit_place.addEventListener("click", function() {
-    r.open("GET", "http://www.openweathermap.org/data/2.5/forecast?id=" + place.value + "&appid=b6907d289e10d714a6e88b30761fae22", true);
+    r.open("GET", "http://www.openweathermap.org/data/2.5/forecast?id=" + place.value + "&appid=e635e6a8c683f448b994a6e254567aa7", true);
     r.send();
     for (var i = 0; i <= 5; i++) {
-        document.getElementsByClassName("span_hum")[i].innerHTML = "Humidity: "
+        spanHumidity[i].innerHTML = "Humidity: "
     }
 });
+
+
+r.open("GET", "http://www.openweathermap.org/data/2.5/forecast?id=" + place.value + "&appid=e635e6a8c683f448b994a6e254567aa7", true);
+r.send();
+
+for (var i = 0; i <= 5; i++) {
+    spanHumidity[i].innerHTML = "Humidity: "
+}
 // 1591449 bắc ninh
 // 1581130 hà nội
 // 1566083 tp.hcm
-// r.open("GET", "http://www.openweathermap.org/data/2.5/forecast?id=" + place.value + "&appid=b6907d289e10d714a6e88b30761fae22", true);
 
